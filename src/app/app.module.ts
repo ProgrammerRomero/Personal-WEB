cb5a06173a2967fa206a9112e58a836680fb7f48
programmer <0xprogrammerx0@gmail.com>
2020-05-04 18:59:59 +1000

    Components: skills, projects, about, contactMe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContentComponent } from './components/content/content.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ContentComponent
+    ContentComponent,
+    AboutComponent,
+    ContactMeComponent,
+    ProjectsComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
